4e235bc260429bcf4ce49d85c5d47aa13b94558f
ousseynoukone <ousseynou781227@gmail.com>
2024-06-21 17:19:30 +0000

    update

--- src/controllers/cronJobs/deleteOldDeactivatedAccount.js
+++ src/controllers/cronJobs/deleteOldDeactivatedAccount.js
@@ -2,26 +2,29 @@ const { Op } = require("sequelize");
 const { sequelize } = require("../../db/db");
 const User = require("../../models/User");
 
-// Clean up expired blacklisted tokens
 async function cleanupOldDeactivatedAccount() {
   const t = await sequelize.transaction();
   try {
-    const user = await User.findOne({where:{email:req.user.email}})
+    // Calculate the date 10 minutes ago from now
+    const tenMinutesAgo = new Date();
+    tenMinutesAgo.setMinutes(tenMinutesAgo.getMinutes() - 10);
 
-
-    // Delete tokens where the current time exceeds the createdAt time plus the expiration seconds
+    // Delete users where isActive is false and createdAt is older than 10 minutes
     const deletedCount = await User.destroy({
       where: {
-        isActive:false
+        isActive: false,
+        createdAt: {
+          [Op.lt]: tenMinutesAgo
+        }
       },
       transaction: t
     });
 
     await t.commit();
-    console.log(`Deleted ${deletedCount} old Deactivated Account(s).`);
+    console.log(`Deleted ${deletedCount} old deactivated account(s).`);
   } catch (error) {
     await t.rollback();
-    console.error('Error cleaning up Old Deactivated Account(s)', error);
+    console.error('Error cleaning up old deactivated account(s)', error);
   }
 }
 
